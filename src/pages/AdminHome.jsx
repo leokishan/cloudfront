64ec9595ecb3a53f215138da4f4839a7d43cbd56
Arunkumar Subash Gauda <ar822955@dal.ca>
2021-12-09 23:59:58 -0400

    Admin&Servicefrontpages

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,21 +1,31 @@
 // import React from "react"
-import React, {Component} from "react"
+import React, {useState, useEffect} from "react"
 import {Button, ListGroup, Badge} from 'react-bootstrap'
 import Card from "react-bootstrap/Card";
+import axios from "axios"
 
 const AdminHome = () => {
+const { REACT_APP_API_URL } = process.env
+const [userList, setUserList] = useState([])
+useEffect(() => {
+  axios.get(`${REACT_APP_API_URL}/user/users`).then(res => {
+    setUserList(res.data.users || [])
+  }).catch(e => console.log(e))
+}, [])
+
   return (
-    <div>
+  <div class="d-flex">
+    { userList.map(ele => (
       <Card style={{ width: '18rem' , margin:"2%", padding:"15px"}}>
-      <div>
-      <Card.Img variant="top" src="holder.js/100px180" />
-        <p className="mt-3"><b>User Name </b> (User Type)</p>
-        <p class="text-secondary">Email</p>
-        <p className="text-center"><Button style={{backgroundColor: "red"}}>Remove User</Button></p>
-  </div>
-</Card>
-</div>
-  )
+        <div>
+        <Card.Img variant="top" src="holder.js/100px180" />
+          <p className="mt-3"><b>{ele.user_name} </b> ({ele.user_type})</p>
+          <p class="text-secondary">{ele.user_email}</p>
+          <p className="text-center"><Button style={{backgroundColor: "red"}}>Remove User</Button></p>
+        </div>
+    </Card>
+    ))}
+  </div>)
 }
 
 export default AdminHome
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,23 +1,32 @@
-import React, {Component} from "react"
+import React, {useState, useEffect} from "react"
 import {Button, ListGroup, Badge} from 'react-bootstrap'
 import Card from "react-bootstrap/Card";
+import axios from "axios"
 
 
 const Services = () => {
+  const { REACT_APP_API_URL } = process.env
+  const [ServiceList, setServiceList] = useState([])
+    useEffect(() => {
+  axios.get(`${REACT_APP_API_URL}/service/services`).then(res => {
+    setServiceList(res.data.users || [])
+  }).catch(e => console.log(e))
+}, [])
   return (
-    <div>
-    <Card style={{ width: '18rem' , margin:"2%", padding:"15px"}}>
-      <div>
-    <p className="mt-3"><b>Service Type: </b></p>
-    <p>Client Name: </p>
-    <p>Service Provider:  </p>
-    <p>Service Status: </p>
-    <p>Serive Charge: </p>
-    <Badge pill bg="secondary">Status</Badge>{' '}
-    <p className="text-center"><Button style={{backgroundColor: "blue"}}>Delete</Button></p>
+    <div class="d-flex">
+      { ServiceList.map(ele => (
+      <Card style={{ width: '18rem' , margin:"2%", padding:"15px"}}>
+        <div>
+          <p className="mt-3"><b>{ele.service_type}</b></p>
+          <p>{ele.service_clientname}</p>
+          <p>{ele.service_provider}</p>
+          <p>{ele.service_tatus}</p>
+          <p>{ele.service_charge}</p>
+          <Badge pill bg="secondary">Status</Badge>{' '}
+          <p className="text-center"><Button style={{backgroundColor: "blue"}}>Delete</Button></p>
+        </div>
+      </Card>))}
     </div>
-</Card>
-</div>
   )
 }
 
