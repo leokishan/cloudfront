4775d2e4449b2e8c096fb6f33dbf82eb45f821fa
Kishan Thakkar <thakkarkishan097@gmail.com>
2021-12-09 22:48:18 -0400

    add user details to dynamodb

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/signup.module.css";
 import { Auth } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+
+const { REACT_APP_API_URL } = process.env
 
 const Signup = (props) => {
+  const [departmentList, setDepartmentList] = useState([])
   const [values, setValues] = useState({
+    name: "",
+    department: "",
     Email: "",
     Password: "",
     Username: "",
@@ -15,6 +21,12 @@ const Signup = (props) => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
+  useEffect(() => {
+    axios.get(`${REACT_APP_API_URL}/department/departments`).then(res => {
+      setDepartmentList(res.data.departments || [])
+    }).catch(e => console.log(e))
+  }, [])
+
   const handleChange = (e) =>
     setValues({ ...values, [e.target.name]: e.target.value });
 
@@ -22,9 +34,10 @@ const Signup = (props) => {
     if (
       !values.Password||
       !values.Username ||
+      !values.name ||
       !values.Email
     ) {
-      setError("Email, username and password are mandatory to fill!");
+      setError("Email, username, name and password are mandatory to fill!");
       return;
     }
     if (values.Password !== values.ConfirmPassword) {
@@ -37,9 +50,19 @@ const Signup = (props) => {
       password: values.Password,
       attributes: {
         email: values.Email,
+        name: values.name,
         "custom:userType": values.loginType,
       },
-    }).then((res) => {
+    }).then(async(res) => {
+      let departmentObj = departmentList.find(ele => ele.department_id === values.department)
+      await axios.post(`${REACT_APP_API_URL}/user/adduser`, {
+        user_id: res.userSub,
+        user_type: values.loginType,
+        user_name: values.Username,
+        user_email: values.Email,
+        department_id: values.department,
+        department_name: departmentObj.department_name,
+      })
       setLoading(false)
       navigate("/login")
     }).catch((err) => {
@@ -64,6 +87,16 @@ const Signup = (props) => {
           />
         </div>
         <div>
+          Name
+          <br />
+          <input
+            type="text"
+            name="name"
+            className={styles.input}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
           Email
           <br />
           <input
@@ -78,13 +111,27 @@ const Signup = (props) => {
           <br />
           <select
             name="loginType"
-            className={`form-control ${styles.input} ${styles.select}`}
+            className={`form-control mt-2 ${styles.input} ${styles.select}`}
             onChange={handleChange}
           >
             <option value="ServiceProvider">Service Provider</option>
             <option value="Client">Client</option>
           </select>
         </div>
+        {values.loginType === "ServiceProvider" ? <div>
+          Department
+          <br />
+          <select
+            name="department"
+            className={`form-control mt-2 ${styles.input} ${styles.select}`}
+            onChange={handleChange}
+          >
+            <option value="">Select Option</option>
+            {departmentList.map(ele => (
+              <option key={ele.department_id} value={ele.department_id}>{ele.department_name}</option>
+            ))}
+          </select>
+        </div> : <div />}
         <div />
         <div>
           Password
